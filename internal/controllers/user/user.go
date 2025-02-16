b19c3f2bd0ec87797b20dcb620b80c287f9128aa
icelain <icelain@ice.computer>
2025-02-16 17:28:34 +0530

    finish image to latex conversion on user controller

--- internal/controllers/user/user.go
+++ internal/controllers/user/user.go
@@ -1,6 +1,7 @@
 package user
 
 import (
+	"encoding/json"
 	"icealpha/internal/router"
 
 	"net/http"
@@ -10,9 +11,9 @@ func HandleSolveInputImage(pattern string, rtr *router.Router) {
 
 	rtr.R.Post(pattern, func(w http.ResponseWriter, r *http.Request) {
 
-		file := r.MultipartForm.File["problem"][0]
+		multiPartFile := r.MultipartForm.File["problem"][0]
 
-		if file.Size > (1024 * 1024 * 20) { // 20 mb
+		if multiPartFile.Size > (1024 * 1024 * 20) { // 20 mb
 
 			http.Error(w, "file size exceeded", http.StatusBadRequest)
 			return
@@ -20,6 +21,34 @@ func HandleSolveInputImage(pattern string, rtr *router.Router) {
 		}
 
 		// add checks to filter out non-image files
+
+		file, err := multiPartFile.Open()
+		if err != nil {
+
+			http.Error(w, "error opening submitted image", http.StatusInternalServerError)
+			return
+
+		}
+
+		latex, err := rtr.S.ImgLatex.ImageToLatex(file)
+		if err != nil {
+
+			http.Error(w, "could not extrapolate information from given file", http.StatusBadRequest)
+			return
+
+		}
+
+		response := map[string]string{
+
+			"content": latex,
+		}
+
+		if err = json.NewEncoder(w).Encode(response); err != nil {
+
+			http.Error(w, "Error writing final response", http.StatusInternalServerError)
+			return
+
+		}
 
 	})
 
