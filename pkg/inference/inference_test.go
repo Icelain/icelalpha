2cc16c62d019d5fa6af88832c9f6f10887f758d6
icelain <icelain@ice.computer>
2025-02-18 18:19:49 +0530

    add testing to inference package

--- pkg/inference/inference_test.go
+++ pkg/inference/inference_test.go
@@ -1,9 +1,36 @@
 package inference
 
 import (
+	"context"
 	"testing"
 )
 
-func TestStreamResponse(t *testing.T) {
+func TestClaudeStreamResponse(t *testing.T) {
+
+	// pass by default
+
+}
+
+func TestOllamaDeepseekStreamResponse(t *testing.T) {
+
+	ollamaClient, err := NewOllamaLLMClient(context.Background(), "deepseek-r1:14b")
+	if err != nil {
+
+		t.Fatal("error creating ollama client", err)
+
+	}
+
+	respchan, err := ollamaClient.StreamResponse(context.Background(), "what is the derivative of ln(x)? Only and only give the answer to the question and do not attempt to say anything else")
+	if err != nil {
+
+		t.Fatal("error creating stream response from ollama client with model: ", ollamaClient.model)
+
+	}
+
+	for msg := range respchan {
+
+		t.Log("output tokens: ", msg)
+
+	}
 
 }
