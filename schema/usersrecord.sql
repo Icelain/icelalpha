52ba3a069a22ab84c12ece285b89adbf7437ac74
icelain <icelain@ice.computer>
2025-03-08 18:43:12 +0530

    update schema

--- schema/usersrecord.sql
+++ schema/usersrecord.sql
@@ -1,1 +1,1 @@
-create table if not exists usersrecord(id uuid primary key default uuid_generate_v4() not null, username text, email text unique);
+create table if not exists usersrecord(id uuid primary key default uuid_generate_v4() not null, username text, email text unique, credits int default 0 not null);
