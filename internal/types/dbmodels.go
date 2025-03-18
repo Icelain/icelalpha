5a8c5ee530367c05b2df0254aadfc2f5179f024d
icelain <icelain@ice.computer>
2025-03-18 20:56:51 +0530

    add comments

--- internal/types/dbmodels.go
+++ internal/types/dbmodels.go
@@ -6,8 +6,10 @@ import (
 
 // Icealpha user schema
 type User struct {
-	UUID          uuid.UUID
-	Email         string
-	Username      string
+	UUID     uuid.UUID
+	Email    string
+	Username string
+
+	// keep track of credits expended by the user
 	CreditBalance uint64
 }
