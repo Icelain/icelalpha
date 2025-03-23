a55cdadc36ebe305020e15fe5db3d21160fd615d
icelain <icelain@ice.computer>
2025-03-23 15:53:02 +0530

    implement credit syncing

--- internal/database/creditsync.go
+++ internal/database/creditsync.go
@@ -1,10 +1,26 @@
 package database
 
-// import (
-// 	"context"
-// 	"sync"
-// )
+import (
+	"context"
+	"sync"
+)
 
-// func Sync(postgresDriver *PostgresDriver, cache *sync.Map) {
+func Sync(postgresDriver *PostgresDriver, cache *sync.Map) error {
 
-// }
+	var err error
+
+	cache.Range(func(key, value any) bool {
+
+		if err = postgresDriver.UpdateUserCredits(context.Background(), key.(string), value.(uint)); err != nil {
+
+			return false
+
+		}
+
+		return true
+
+	})
+
+	return err
+
+}
