4ad971e2ebe42a851f832715fa9db7a69f180215
icelain <icelain@ice.computer>
2025-03-30 14:25:24 +0530

    remove cookies from state

--- internal/state/state.go
+++ internal/state/state.go
@@ -5,12 +5,9 @@ import (
 	"icealpha/pkg/imglatex"
 	"icealpha/pkg/inference"
 	"sync"
-
-	"github.com/gorilla/sessions"
 )
 
 type State struct {
-	CookieStore *sessions.CookieStore
 	DB          *database.PostgresDriver
 	CreditCache *sync.Map
 	ImgLatex    *imglatex.ImgLatex
